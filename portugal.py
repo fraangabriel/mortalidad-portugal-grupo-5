91738381a851c21a402e4f4fe33b00e9a20f8460
fraangabriel <franciscogabrielnet@gmail.com>
2025-10-21 16:17:14 -0400

    🐍 Estructura Parte 2
    Aplicación inicial de la estructura y cálculos por grupos

--- portugal.py
+++ portugal.py
@@ -67,3 +67,65 @@ def epoca_economica(year):
         return '2020–2023: pandemia y reconfiguración demográfica'
     else:
         return 'Otro' # Para años fuera de los rangos definidos
+
+# E3
+def gCenso(edad):
+    if 0 <= edad <= 14:
+        return '0-14 años'
+    elif  15 <= edad <= 64:
+        return '15-64 años'
+    elif 65 <= edad:
+        return '65 años y más'
+    else:
+        return 'Ninguna otra etapa' # Para años fuera de los rangos definidos
+
+# E4
+def etapa(edad):
+    if 0 <= edad <= 5:
+        return 'Infancia'
+    elif  6 <= edad <= 11:
+        return 'Ninez'
+    elif 12 <= edad <= 19:
+        return 'Pubertad y Adolescencia'
+    elif 20 <= edad <= 39:
+        return 'Adultos jóvenes'
+    elif 40 <= edad <= 49:
+        return 'Adultos intermedios'
+    elif 50 <= edad <= 59:
+        return 'Adultos maduros'
+    elif 60 <= edad <= 69:
+        return 'Viejos incipientes'
+    elif 70 <= edad <= 84:
+        return 'Viejos intermedios'
+    elif 85 <= edad:
+        return 'Viejos avanzados'
+    else:
+        return 'Ninguna otra etapa' # Para años fuera de los rangos definidos
+
+
+# función para crear las columnas nuevas
+df0['T3'] = df0['Year'].apply(decada)
+df0['T4'] = df0['Year'].apply(epoca_economica)
+
+df0['E3'] = df0['Age'].apply(gCenso)
+df0['E4'] = df0['Age'].apply(etapa)
+
+# ver si muestra nuevas columnas
+print(df0.head())
+print(df0.tail())
+
+# filtro según E3 para mostrar el promedio mx
+# -------------------------------------------------
+mx_grouped_by_E3 = df0.groupby('E3')['mx'].mean()
+mx_grouped_by_T4 = df0.groupby('T4')['mx'].mean()
+
+# ver el resultado
+print("")
+print("Valor de 'mx' según 'E3':")
+print(mx_grouped_by_E3)
+
+print("")
+print("Valor de 'mx' según 'T4':")
+print(mx_grouped_by_T4)
+
+print(mx_grouped_by_T4.info())
